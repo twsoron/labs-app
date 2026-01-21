f2cadf19442f33fa5a204d87ca0ddcc7d0b01892
twsoron <twsoron@syr.edu>
2026-01-21 19:02:29 +0000

    nav

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+lab_1 = st.Page('lab_1.py', title = "Lab 1")
+lab_2 = st.Page('lab_2.py', title = "Lab 2", default=True)
+
+nav = st.navigation([lab_1, lab_2])
+st.set_page_config(page_title= "Labs", initial_sidebar_state="expanded")
+nav.run()
